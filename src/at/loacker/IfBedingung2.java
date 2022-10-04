227fd7b652dd3f27191c88d62b422ace3129e845
matteohald <matteo.loacker@student.htldornbirn.at>
2022-10-04 10:54:23 +0200

    Zufallszahl2 fertig

---
+++ src/at/loacker/IfBedingung2.java
@@ -0,0 +1,30 @@
+package at.loacker;
+
+import java.util.Random;
+
+class ifBedingung2 {
+
+    public static void main(String[] args) {
+
+        // Erstelle zwei Zufallszahlen zwischen 0 und 100
+
+        Random random = new Random();
+        int randomNumber1 = random.nextInt(100);
+        int randomNumber2 = random.nextInt(100);
+
+        System.out.println(randomNumber1);
+        System.out.println(randomNumber2);
+
+        if (randomNumber1 < randomNumber2 && randomNumber1 < 50) {
+            System.out.println("Zahl 1 ist kleiner als Zahl 2 und Mini");
+        }
+
+        else if (randomNumber1 < 30 || randomNumber2 < 30) {
+            System.out.println("Eine der beiden ist kleiner als 30");
+        }
+
+        else if (randomNumber1 < 50 && randomNumber2 != 50) {
+            System.out.println("Erste Zahl klein, zweite kein 50iger");
+        }
+
+    }}
