8f4e66f7a195008b816030b7606da5d27a05eb01
matteohald <matteo.loacker@student.htldornbirn.at>
2022-10-11 10:14:33 +0200

    Loop 1 fertig

---
+++ src/at/loacker/Loop1.java
@@ -0,0 +1,12 @@
+package at.loacker;
+
+class Loop1 {
+    public static void main(String[] args) {
+        for (int i = 1; i <=1000 ; i++) {
+            System.out.println(i);
+
+        }
+    }
+
+
+}
