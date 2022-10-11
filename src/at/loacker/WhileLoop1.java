567604f48137b7927255edf047ce17e7f0e085fe
matteohald <matteo.loacker@student.htldornbirn.at>
2022-10-11 11:13:40 +0200

    While Loop fertig

---
+++ src/at/loacker/WhileLoop1.java
@@ -0,0 +1,31 @@
+package at.loacker;
+
+import java.util.Random;
+
+class WhileLoop1 {
+    public static void main(String[] args) {
+        boolean finished = false;
+        int result = 0;
+
+        while (!finished){
+            Random random = new Random();
+            int randomNumber1 = 10 + random.nextInt(30 - 10 + 1);
+            int randomNumber2 = 10 + random.nextInt(30 - 10 + 1);
+
+            var summ = randomNumber1 + randomNumber2;
+                System.out.println(randomNumber1 + " & " + randomNumber2);
+                result += summ;
+
+            if (randomNumber1 == 15 || randomNumber2 == 15 ){
+                finished = true;
+                System.out.println("program finished, 15");
+            }
+            else if (randomNumber1 == 25 || randomNumber2 == 25){
+                finished = true;
+                System.out.println("program finished, 25");
+            }
+
+        }
+        System.out.println("Die summe der Zahlen ergibt: " + result);
+    }
+}
