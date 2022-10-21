e33eb43189e2417177258b056130e58f12b8e8e4
matteohald <matteo.loacker@student.htldornbirn.at>
2022-10-21 14:29:55 +0200

    Arrays Aufgabe 2 fertig

---
+++ src/at/loacker/ArraysAufgabe2.java
@@ -0,0 +1,17 @@
+package at.loacker;
+import java.util.Random;
+
+class ArraysAufgabe2 {
+    public static void main(String[] args) {
+
+        int total = 0;
+        int[] array = new int[50];
+        int max = 100;
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int)(Math.random()*max);
+            total = total + array[i];
+            System.out.println(array[i]);
+        }
+        System.out.println("Summe: " + total);
+    }
+}
