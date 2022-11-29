57032694070a724babbd57c0714103c9578704bb
matteohald <matteo.loacker@student.htldornbirn.at>
2022-11-29 11:23:18 +0100

    car object 2

--- src/at/loacker/Car.java
+++ src/at/loacker/Car.java
@@ -3,15 +3,24 @@ package at.loacker;
 class Car {
     public int fuelConsumption;
     public int fuelAmount;
+    public int superboostConsumption;
     public String brand;
     public String serialNumber;
     private String color;
 
     public void drive(){
         this.fuelAmount = this.fuelAmount - fuelConsumption;
-        System.out.println("The car's driving");
+        System.out.println("Ich fahre");
     }
     public void brake(){
-        System.out.println("The car stopped");
+        System.out.println("Ich bremse aus");
+    }
+    public void turboBoost(){
+        if (this.fuelAmount >= fuelAmount * 0.1){
+            this.fuelAmount = this.fuelAmount - superboostConsumption;
+            System.out.println("SuperBoostMode");
+        }else{
+            System.out.println("Not enough fuel for SuperBoostMode");
+        }
     }
 }
--- src/at/loacker/Main.java
+++ src/at/loacker/Main.java
@@ -5,15 +5,17 @@ class Main {
 
         Car c1 = new Car();
         c1.brand = "Audi";
-        c1.fuelConsumption = 7;
+        c1.fuelConsumption = 5;
         c1.serialNumber = "A1234";
         c1.fuelAmount = 70;
+        c1.superboostConsumption = 7;
 
         Car c2 = new Car();
         c2.brand = "Mercedes";
-        c2.fuelConsumption = 6;
+        c2.fuelConsumption = 4;
         c2.serialNumber = "B4321";
-        c2.fuelAmount = 20;
+        c2.fuelAmount = 50;
+        c2.superboostConsumption = 5;
 
         System.out.println(c2.fuelAmount);
         c2.drive();
@@ -23,7 +25,8 @@ class Main {
         c1.drive();
         c1.brake();
         System.out.println(c1.fuelAmount);
-
+        c1.turboBoost();
+        System.out.println(c1.fuelAmount);
 
 
 
