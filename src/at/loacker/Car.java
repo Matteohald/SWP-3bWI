b08eab9167c820f54886edc69c52af8ecb9be07b
matteohald <matteo.loacker@student.htldornbirn.at>
2022-12-16 14:45:32 +0100

    car Erweiterung fertig

--- src/at/loacker/Car.java
+++ src/at/loacker/Car.java
@@ -9,35 +9,50 @@ class Car {
     public String brand;
     public String serialNumber;
     private String color;
+    public Car(int fuelConsumption, String brand, String serialNumber){
+        this.fuelConsumption = fuelConsumption;
+        this.brand = brand;
+        this.serialNumber = serialNumber;
 
-    public void drive(){
+    }
+
+    public Car() {
+
+    }
+
+
+    public void drive() {
         this.fuelAmount = this.fuelAmount - fuelConsumption;
         System.out.println("Ich fahre");
     }
-    public void brake(){
+
+    public void brake() {
         System.out.println("Ich bremse aus");
     }
-    public void turboBoost(){
-        if (this.fuelAmount >= fuelAmount * 0.1){
+
+    public void turboBoost() {
+        if (this.fuelAmount >= fuelAmount * 0.1) {
             this.fuelAmount = this.fuelAmount - superboostConsumption;
             System.out.println("SuperBoostMode");
-        }else{
+        } else {
             System.out.println("Not enough fuel for SuperBoostMode");
         }
     }
 
-    public void honk(){
+    public void honk() {
         System.out.println("How often should be honked?");
         Scanner scanner = new Scanner(System.in);
         int honkAmount = scanner.nextInt();
 
-        for(int amountOfRepetitions = 0; amountOfRepetitions < honkAmount; amountOfRepetitions++){
+        for (int amountOfRepetitions = 0; amountOfRepetitions < honkAmount; amountOfRepetitions++) {
             System.out.println("Tuuut");
         }
     }
 
-    public void getRemainingRange(){
+    public void getRemainingRange() {
         int range = this.fuelAmount / this.fuelConsumption;
         System.out.println("You can still drive " + range + " times before you have to refuel");
     }
+
+
 }
